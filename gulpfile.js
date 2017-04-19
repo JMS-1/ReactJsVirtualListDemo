28ca37eca459d2e4b892f22b1e88d5e10f86e6c1
Dr. Jochen Manns <vcrnet-1@psimarron.net>
2017-04-19 18:33:36 +0200

    Added SASS

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+﻿const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+gulp.task('build', function () {
+    gulp.src('src\\index.scss').pipe(sass()).pipe(gulp.dest('.'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('src\\**\\*.scss', ['build']);
+});
