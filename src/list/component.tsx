292d1f74cbe26bb6c9c0bb438b41d4eeb2febc50
Dr. Jochen Manns <vcrnet-1@psimarron.net>
2017-04-20 00:46:16 +0200

    Using view model

--- src/list/component.tsx
+++ src/list/component.tsx
@@ -1,36 +1,23 @@
 ﻿import * as React from 'react';
 
-interface IListProps {
-    itemHeight: number;
+import ViewModel, { IVirtualList, IVirtualListSite, IVirtualListItems, IVirtualListSlider, IViewModelProps } from './viewModel';
+
+interface INoState {
 }
 
-interface IInternalListProps {
-    relStart: number;
-
-    onCountChanged(visible: number, total: number): void;
-}
-
-interface IListState {
-    count: number;
-
-    total: number;
-}
-
-class List extends React.PureComponent<IListProps & IInternalListProps, IListState>{
+class List extends React.Component<IViewModelProps<IVirtualListItems>, INoState>{
     private _outer: HTMLDivElement;
 
     render(): JSX.Element {
         const children = React.Children.toArray(this.props.children) || [];
-        const start = this.props.relStart * children.length;
-        const count = this.state ? this.state.count : 0;
 
-        return <div className='istk-list-items' ref={e => this._outer = e}>{children.slice(start, start + count)}</div>;
+        return <div className='istk-list-items' ref={e => this._outer = e}>
+            {children.slice(this.props.vm.start, this.props.vm.start + this.props.vm.count)}
+        </div>;
     }
 
     componentWillMount(): void {
-        const children = React.Children.toArray(this.props.children) || [];
-
-        this.setState({ total: children.length });
+        this.props.vm.setTotal(React.Children.count(this.props.children));
 
         window.addEventListener('resize', this.recalculateCount);
     }
@@ -43,41 +30,15 @@ class List extends React.PureComponent<IListProps & IInternalListProps, IListSta
         this.recalculateCount();
     }
 
-    private recalculateCount = () => {
-        const size = this._outer.clientHeight;
-        const count = Math.ceil(size / (this.props.itemHeight));
-        const stateCount = this.state ? this.state.count : -1;
-
-        if (count === stateCount)
-            return;
-
-        this.setState({ count });
-
-        this.props.onCountChanged(count, this.state.total);
-    }
+    private recalculateCount = () => this.props.vm.setHeight(this._outer.clientHeight);
 }
 
-interface IScrollProps {
-    relKnobSize: number;
-
-    relKnobPos: number;
-
-    moveTo(relPos: number): void;
-}
-
-interface IScrollState {
-}
-
-class Scroll extends React.PureComponent<IScrollProps, IScrollState>{
+class Scroll extends React.Component<IViewModelProps<IVirtualListSlider>, INoState>{
     render(): JSX.Element {
-        const relKobPos = Math.min(this.props.relKnobPos, 100 - this.props.relKnobSize);
-
-        return <div className='istk-list-scroll' onClick={this.onClick}><div /><div style={{ height: `${this.props.relKnobSize}%`, top: `${relKobPos}%` }} /></div>;
+        return this.props.vm && <div className='istk-list-scroll' onClick={this.onClick}><div /><div style={{ height: `${this.props.vm.sliderHeight}%`, top: `${this.props.vm.sliderPosition}%` }} /></div>;
     }
 
     private onClick = (ev: React.MouseEvent<HTMLDivElement>) => {
-        ev.preventDefault();
-
         const y = ev.pageY - ev.currentTarget.offsetTop;
         if (y < 0)
             return;
@@ -86,35 +47,36 @@ class Scroll extends React.PureComponent<IScrollProps, IScrollState>{
         if (y > h)
             return;
 
-        this.props.moveTo(y / h);
+        this.props.vm.setPosition(y / h);
     }
 }
 
-interface IInternalListState {
-    relStart: number;
+interface IListProps {
+    itemHeight: number;
 }
 
-export default class extends React.PureComponent<IListProps, IListState & IInternalListState> {
-    render(): JSX.Element {
-        const relKnobSize = (this.state && (this.state.total !== undefined) && (this.state.total > 0)) ? Math.max(1, Math.min(100, 100 * this.state.count / this.state.total)) : 0;
-        const relStart = Math.min(1 - relKnobSize / 100, (this.state && this.state.relStart) || 0);
-        const relKnobPos = (this.state && (this.state.total !== undefined) && (this.state.total > 0)) ? Math.max(0, Math.min(100, 100 * Math.floor(relStart * this.state.total) / this.state.total)) : 0;
+export default class extends React.Component<IListProps, INoState> {
+    private _viewModel: IVirtualList;
 
+    render(): JSX.Element {
         return <div className='istk-list'>
             <div>
-                <List {...this.props} relStart={relStart} onCountChanged={this.setCount} />
-                <Scroll moveTo={this.moveTo} relKnobSize={relKnobSize} relKnobPos={relKnobPos} />
+                <List vm={this._viewModel.itemsViewModel} children={this.props.children} />
+                <Scroll vm={this._viewModel.sliderViewModel} />
             </div>
         </div>;
     }
 
-    private setCount = (count: number, total: number) => {
-        if (!this.state || (count !== this.state.count) || (total !== this.state.total))
-            this.setState({ count, total });
-    };
+    componentWillMount(): void {
+        const refresh = this.forceUpdate.bind(this);
 
-    private moveTo = (relStart: number) => {
-        if (!this.state || (relStart !== this.state.relStart))
-            this.setState({ relStart });
+        this._viewModel = new ViewModel(this.props.itemHeight, { refresh });
+    }
+
+    componentWillUnmount(): void {
+        if (this._viewModel) {
+            this._viewModel.disconnect();
+            this._viewModel = undefined;
+        }
     }
 }
---
+++ src/list/viewModel.ts
@@ -0,0 +1,128 @@
+﻿export interface IViewModelProps<TViewModelType> {
+    readonly vm: TViewModelType;
+}
+
+export interface IVirtualListSite {
+    refresh(): void;
+}
+
+export interface IVirtualList {
+    readonly itemsViewModel: IVirtualListItems;
+
+    readonly sliderViewModel: IVirtualListSlider;
+
+    disconnect(): void;
+}
+
+export interface IVirtualListItems {
+    readonly start: number;
+
+    readonly count: number;
+
+    setTotal(newTotal: number): void;
+
+    setHeight(newHeight: number): void;
+}
+
+export interface IVirtualListSlider {
+    setPosition(relPosition: number): void;
+
+    readonly sliderHeight: number;
+
+    readonly sliderPosition: number;
+}
+
+export default class implements IVirtualList, IVirtualListItems, IVirtualListSlider {
+    constructor(private readonly _itemHeight: number, private _site: IVirtualListSite) {
+    }
+
+    disconnect(): void {
+        this._site = undefined;
+    }
+
+    get itemsViewModel(): IVirtualListItems {
+        return this;
+    }
+
+    get sliderViewModel(): IVirtualListSlider {
+        return (this.count < this._total) ? this : null;
+    }
+
+    // Die Anzahl der Elemente.
+    private _total = 0;
+
+    // Legt die gesamte Anzahl der Elemente fest.
+    setTotal(newTotal: number): void {
+        // Das wissen wir schon.
+        if (newTotal === this._total)
+            return;
+
+        // Wert übernehmen.
+        this._total = newTotal;
+
+        // Anzeige aktualisieren.
+        this._site.refresh();
+    }
+
+    // Die laufende Nummer des ersten anzuzeigenden Elementes.
+    start = 0;
+
+    // Meldet eine neue Position des Schiebers.
+    setPosition(relPosition: number): void {
+        // Relative Position in das zughörige Element umrechnen - wir achten darauf, dass der Schieber nicht aus der Liste rutscht.
+        var start = Math.max(0, Math.min(this._total - 1, Math.floor(Math.min(1 - this.sliderHeight / 100, relPosition) * this._total)));
+
+        // Es hat nicht nichts verändert.
+        if (start === this.start)
+            return;
+
+        // Neuen Wert übernehmen.
+        this.start = start;
+
+        // Anzeige erneuern.
+        this._site.refresh();
+    }
+
+    // Die Anzahl der angezeigten Elemente.
+    count = 0;
+
+    // Setzt die Höhe der Liste.
+    setHeight(newHeight: number): void {
+        // Wieviele Elemente passen da hinein?
+        const count = Math.ceil(newHeight / (this._itemHeight));
+
+        // Es hat sich nichts verändert.
+        if (count === this.count)
+            return;
+
+        // Neue Anzahl merken.
+        this.count = count;
+
+        // Eventuell verrutscht der Schieber nun noch.
+        if (this._total > 0)
+            this.setPosition(this.start / this._total);
+
+        // Anzeige auf jeden Fall aktualisieren.
+        this._site.refresh();
+    }
+
+    // Ermittelt die relative Größe des Schiebers.
+    get sliderHeight(): number {
+        // Wenn wir keine Daten haben geht der Schieber über den gesamten Bereich.
+        if (this._total < 1)
+            return 100;
+
+        // Die Größe des Schiebers ergibt sich leicht aus der angezeigten Anzahl von Elementen.
+        return Math.max(1, Math.min(100, 100 * this.count / this._total));
+    }
+
+    // Ermittelt die relative Position des Schiebers.
+    get sliderPosition(): number {
+        // Wir haben gar keine Daten.
+        if (this._total < 1)
+            return 0;
+
+        // Der Schieber verläßt niemals den Bereich des Balkens, ansonsten ist die Berechnung trivial.
+        return 100 * Math.min(1 - this.sliderHeight / 100, this.start / this._total);
+    }
+}
