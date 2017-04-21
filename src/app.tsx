50a18fb166223b80e074e44d55506715cacf64fd
Dr. Jochen Manns <vcrnet-1@psimarron.net>
2017-04-21 16:52:14 +0200

    Test data

--- src/app.tsx
+++ src/app.tsx
@@ -11,7 +11,7 @@ interface IRootState {
 
 const names = [];
 
-for (let i = 0; i++ < 10;)
+for (let i = 0; i++ < 1000;)
     names.push(`Item ${i}`);
 
 class Root extends React.PureComponent<IRootProps, IRootState>
